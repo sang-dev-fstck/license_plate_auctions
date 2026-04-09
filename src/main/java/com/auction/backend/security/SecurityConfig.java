4757560d4f50cce5abd86ecb9844b264c9b770a3
Sang <sanghn.dev@gmail.com>
2026-04-09 17:42:13 +0700

    feat (authentication): set up

--- src/main/java/com/auction/backend/security/SecurityConfig.java
+++ src/main/java/com/auction/backend/security/SecurityConfig.java
@@ -1,15 +1,52 @@
 package com.auction.backend.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    // Inject Anh thợ mỏ vào đây
+    private final CustomUserDetailsService customUserDetailsService;
+
+    // Thuật toán Băm (Hash) mật khẩu một chiều: BCrypt (Chuẩn an toàn hiện nay)
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // BƯỚC NÂNG CẤP: Khai báo tường minh "Băng chuyền" kiểm tra mật khẩu
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        // 1. Chỉ định anh thợ mỏ
+        authProvider.setUserDetailsService(customUserDetailsService);
+        // 2. Chỉ định cỗ máy giải mã
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    // AuthenticationManager: Người điều phối việc đăng nhập
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    // Cấu hình màng lọc an ninh chính
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
